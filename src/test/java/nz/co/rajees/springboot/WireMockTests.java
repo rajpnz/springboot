5ccd1c3fbab9fad2d3d048f6b7b43e8d2c88ff48
rajpnz <rajpnz@gmail.com>
2018-01-26 15:47:50 +1300

    Add another wiremock test

--- src/test/java/nz/co/rajees/springboot/WireMockTests.java
+++ src/test/java/nz/co/rajees/springboot/WireMockTests.java
@@ -5,26 +5,18 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
-
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
-import com.github.tomakehurst.wiremock.http.Request;
-import com.github.tomakehurst.wiremock.http.RequestListener;
-import com.github.tomakehurst.wiremock.http.Response;
-import com.github.tomakehurst.wiremock.junit.Stubbing;
-import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.mockito.internal.util.io.IOUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class WireMockTests {
 
@@ -43,13 +35,24 @@ public class WireMockTests {
         HttpGet request = new HttpGet("http://localhost:8089/baeldung/wiremock");
         HttpResponse httpResponse = httpClient.execute(request);
         String stringResponse = convertHttpResponseToString(httpResponse);
-        System.out.println(stringResponse);
         verify(getRequestedFor(urlEqualTo("/baeldung/wiremock")));
         assertEquals(200, httpResponse.getStatusLine().getStatusCode());
         assertEquals("application/json", httpResponse.getFirstHeader("Content-Type").getValue());
         assertEquals("\"testing-library\": \"WireMock\"", stringResponse);
     }
 
+
+    @Test
+    public void testWireMockUsingRajApproach() throws IOException {
+
+        givenThat(get(urlEqualTo("/rule/test")).willReturn(aResponse().withBody("Rule test body")));
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:8089/rule/test");
+        HttpResponse httpResponse = httpClient.execute(request);
+        String stringResponse = convertHttpResponseToString(httpResponse);
+        assertThat(stringResponse, is(Matchers.equalTo("Rule test body")));
+
+    }
 
     //helper methods
     private String convertHttpResponseToString(HttpResponse httpResponse) throws IOException {
