87168a61c4c4f0756e84916ec47e5f82dc4f34e1
rajpnz <rajpnz@gmail.com>
2018-08-03 09:59:15 +1200

    Try using Spring Expression Language (SPEL)

--- src/test/java/nz/co/rajees/springboot/SpringbootApplicationTests.java
+++ src/test/java/nz/co/rajees/springboot/SpringbootApplicationTests.java
@@ -1,7 +1,9 @@
 package nz.co.rajees.springboot;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootApplicationTests {
 
+	@Value("#{${my_prop} + 25}")
+	private int myProp;
+
+	@Value("${my_name}")
+	private String myName ;
+
+	/**
+	 * Example of using a static method in Spring Expression Language (spel)
+	 */
+	@Value("#{T(org.apache.commons.lang3.StringUtils).length('${my_name}')}")
+	private int myNameLength;
+
+	/**
+	 * Example of using a literal property place holder
+	 */
+	@Value("#{'${my_name}'.length() + 19}")
+	private int myNameLength2;
+
 	@Test
 	public void contextLoads() {
+		System.out.println("Hello");
 	}
 
 }
