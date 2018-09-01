7217408fadd72ad06ffb11c1cd3b83c7ef10e7bb
rajpnz <rajpnz@gmail.com>
2018-09-02 09:57:38 +1200

    Specify the name of the property that will toggle the feature

--- src/main/java/nz/co/rajees/springboot/features/AppFeature.java
+++ src/main/java/nz/co/rajees/springboot/features/AppFeature.java
@@ -1,6 +1,7 @@
 package nz.co.rajees.springboot.features;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.ActivationParameter;
 import org.togglz.core.annotation.DefaultActivationStrategy;
 import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
@@ -9,11 +10,15 @@ import org.togglz.spring.activation.SpringEnvironmentPropertyActivationStrategy;
 
 /**
  * Use Togglz to enable features in this application
+ *
+ * @see <a href="https://www.baeldung.com/spring-togglz"></a>
+ * @see <a href="https://medium.com/@bogdandraghicescu/spring-boot-feature-toggles-fe66cfa7504d"></a>
  */
 public enum AppFeature implements Feature {
 
     @Label("Activates the home feature")
-    @DefaultActivationStrategy(id= SpringEnvironmentPropertyActivationStrategy.ID)
+    @DefaultActivationStrategy(id= SpringEnvironmentPropertyActivationStrategy.ID,
+        parameters = {@ActivationParameter(name = "name", value="home_feature")})
     @EnabledByDefault
     HOME_FEATURE;
 
