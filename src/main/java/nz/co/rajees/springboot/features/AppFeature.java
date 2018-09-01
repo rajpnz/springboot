4092b93b99fca7b0707b3e2d9187f63757291069
rajpnz <rajpnz@gmail.com>
2018-09-02 09:43:53 +1200

    Use springboot environment activation strategy

--- src/main/java/nz/co/rajees/springboot/features/AppFeature.java
+++ src/main/java/nz/co/rajees/springboot/features/AppFeature.java
@@ -1,16 +1,19 @@
 package nz.co.rajees.springboot.features;
 
 import org.togglz.core.Feature;
+import org.togglz.core.annotation.DefaultActivationStrategy;
 import org.togglz.core.annotation.EnabledByDefault;
 import org.togglz.core.annotation.Label;
 import org.togglz.core.context.FeatureContext;
+import org.togglz.spring.activation.SpringEnvironmentPropertyActivationStrategy;
 
 /**
  * Use Togglz to enable features in this application
  */
 public enum AppFeature implements Feature {
 
-    @Label("Activates the secret feature")
+    @Label("Activates the home feature")
+    @DefaultActivationStrategy(id= SpringEnvironmentPropertyActivationStrategy.ID)
     @EnabledByDefault
     HOME_FEATURE;
 
